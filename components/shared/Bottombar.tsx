dc81674fc6ed37018a9c7d88a7d555b5071cfc35
Aaksh Gadekar <an14092000@gmail.com>
2023-09-17 16:29:38 +0530

    Added bottombar for mobile devices md:hidden class is added in bottombar className

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,5 +1,41 @@
-function BottomBar(){
-    return <h1>BottomBar</h1>
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { sidebarLinks } from "@/constants";
+
+function Bottombar() {
+  const pathname = usePathname();
+
+  return (
+    <section className='bottombar'>
+      <div className='bottombar_container'>
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={16}
+                height={16}
+                className='object-contain'
+              />
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
-export default BottomBar;
+export default Bottombar;
