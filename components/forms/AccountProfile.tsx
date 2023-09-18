85a1f8efefe77610dcf0f8c6dd52bbdad01f3388
Aaksh Gadekar <an14092000@gmail.com>
2023-09-18 16:57:35 +0530

    Added patterns in next.config.js in order to allow nextjs app to take images from clerk filled the user values in resolver

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -14,6 +14,9 @@ import {zodResolver} from '@hookform/resolvers/zod';
 import { UserValidation } from "@/lib/validations/user";
 import { z } from "zod";
 import { Button } from "../ui/button";
+import Image from "next/image";
+import { ChangeEvent } from "react";
+import { Textarea } from "../ui/textarea";
 
 interface Props {
     user: {
@@ -33,36 +36,121 @@ function onSubmit(values: z.infer<typeof UserValidation>) {
     console.log(values)
 }
 
-const AccountProfile = async ({user, btnTitle}: Props) => {
+const handleImage = (e: ChangeEvent, fieldChange: (value: String) => void) => {
+    e.preventDefault();
+}
+
+const AccountProfile = ({user, btnTitle}: Props) => {
     const form = useForm({
         resolver: zodResolver(UserValidation), 
         defaultValues:{
-            profile_photo: '',
-            name: '',
-            username: '',
-            bio: ''
+            profile_photo: user?.image || "",
+            name: user?.name || "",
+            username: user?.username || "",
+            bio: user?.bio || ""
         }
     });
     return(
         <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <form 
+            onSubmit={form.handleSubmit(onSubmit)} 
+            className="flex flex-col justify-start gap-10">
             <FormField
             control={form.control}
-            name="username"
+            name="profile_photo"
             render={({ field }) => (
-                <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                    <Input placeholder="shadcn" {...field} />
+                <FormItem className="flex items-center gap-4">
+                <FormLabel className="account-form_image-label">
+                    {field.value ? (
+                        <Image 
+                        src={field.value}
+                        alt="profile photo"
+                        width={96}
+                        height={96}
+                        priority
+                        className="rounded-full object-contain"/>
+                    ) :
+                        <Image
+                        src="/assets/profile.svg"
+                        alt="profile photo"
+                        width={24}
+                        height={24}
+                        className="object-contain"/>
+                    }
+                </FormLabel>
+                <FormControl className="flex-1 text-base-semibold text-gray-200">
+                    <Input
+                        type="file"
+                        accept="image/*"
+                        placeholder="Upload a photo"
+                        className="account-form_image-input"
+                        onChange={(e) => handleImage(e, field.onChange)}/>
                 </FormControl>
-                <FormDescription>
-                    This is your public display name.
-                </FormDescription>
                 <FormMessage />
                 </FormItem>
             )}
             />
-            <Button type="submit">Submit</Button>
+
+            <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+                <FormItem className="flex flex-col w-full gap-3">
+                <FormLabel className="text-base-semibold text-light-2">
+                    Name
+                </FormLabel>
+                <FormControl>
+                    <Input
+                        type="text"
+                        className="account-form_input no-focus"
+                        {...field}/>
+                </FormControl>
+                <FormMessage />
+                </FormItem>
+            )}
+            />
+
+            <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+                <FormItem className="flex flex-col w-full gap-3">
+                <FormLabel className="text-base-semibold text-light-2">
+                    Username
+                </FormLabel>
+                <FormControl>
+                    <Input
+                        type="text"
+                        className="account-form_input no-focus"
+                        {...field}/>
+                </FormControl>
+                <FormMessage />
+                </FormItem>
+            )}
+            />
+
+<FormField
+            control={form.control}
+            name="bio"
+            render={({ field }) => (
+                <FormItem className="flex flex-col w-full gap-3">
+                <FormLabel className="text-base-semibold text-light-2">
+                    Bio
+                </FormLabel>
+                <FormControl>
+                    <Textarea
+                        rows={10}
+                        className="account-form_input no-focus"
+                        {...field}/>
+                </FormControl>
+                <FormMessage />
+                </FormItem>
+            )}
+            />
+
+
+
+            <Button type="submit" className="bg-primary-500">Submit</Button>
         </form>
     </Form>
 
