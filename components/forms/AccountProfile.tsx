b803273bd99126c57bf10712550261e727cd4a4c
Aaksh Gadekar <an14092000@gmail.com>
2023-09-18 14:24:27 +0530

    Added form from shadcn library Declare zod resolver for our form imported the necessary libraries

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,4 +1,7 @@
 "use client";
+import {Form} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import {zodResolver} from '@hookform/resolvers/zod';
 
 interface Props {
     user: {
@@ -13,10 +16,11 @@ interface Props {
 }
 
 const AccountProfile = ({user, btnTitle}: Props) => {
+    const form = useForm({
+        resolver: zodResolver()
+    });
     return(
-        <div>
-            Account Profile
-        </div>
+        <Form></Form>
 
     )
 }
